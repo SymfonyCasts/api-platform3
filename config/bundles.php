a0f4d9612eaeef32713a9fd5be880853799412ee
Ryan Weaver <ryan@thatsquality.com>
2023-01-03 15:02:52 -0500

    temporarily adding ai maker bundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfonycasts\AIMakerBundle\AIMakerBundle::class => ['all' => true],
 ];
