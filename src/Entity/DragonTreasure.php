5a21bda8a04a2139f99e5d1c42706d81a9077509
Ryan Weaver <ryan@thatsquality.com>
2023-01-04 11:44:40 -0500

    [tuts-apply-diff] Applying existing diff file for step operations-add-5-core-operations

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -3,13 +3,27 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\DragonTreasureRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
 #[ApiResource(
-    description: 'A rare and valuable treasure.'
+    description: 'A rare and valuable treasure.',
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Patch(),
+        new Delete(),
+    ]
 )]
 class DragonTreasure
 {
