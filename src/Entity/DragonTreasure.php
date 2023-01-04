3278e7843e4e9cd03359a7149973ebee9a275aef
Ryan Weaver <ryan@thatsquality.com>
2023-01-04 11:44:39 -0500

    [tuts-apply-diff] Applying existing diff file for step firstentity-add-apiresource

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\DragonTreasureRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
+#[ApiResource]
 class DragonTreasure
 {
     #[ORM\Id]
