4f866866c0423d9bab5657a3485ff576508eade5
Ryan Weaver <ryan@thatsquality.com>
2023-01-04 15:26:28 -0500

    tuts hero working...

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -44,7 +44,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $password = null;
 
     #[ORM\Column(length: 255, unique: true)]
-    #[Groups(['user:read', 'user:write'])]
+    #[Groups(['user:read', 'user:write', 'treasure:read'])]
     #[Assert\NotBlank]
     private ?string $username = null;
 
