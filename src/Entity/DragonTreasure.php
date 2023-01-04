11a1d4dd4f82e4d860202522baab75c93613bd73
Ryan Weaver <ryan@thatsquality.com>
2023-01-04 14:04:47 -0500

    tuts hero working...

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -31,7 +31,8 @@ use Symfony\Component\Serializer\Annotation\SerializedName;
     ],
     denormalizationContext: [
         'groups' => ['treasure:write'],
-    ]
+    ],
+    paginationItemsPerPage: 10
 )]
 class DragonTreasure
 {
