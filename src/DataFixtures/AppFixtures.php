cfe45b625876c0ee3ed02961a31c30f1fc33971b
Ryan Weaver <ryan@thatsquality.com>
2023-01-04 12:55:59 -0500

    tuts hero working...

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\DragonTreasureFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,6 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        DragonTreasureFactory::createMany(40);
     }
 }
