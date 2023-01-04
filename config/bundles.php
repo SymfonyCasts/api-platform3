2f8224d08ae7dac0b36921b2a059bfe362b1b079
Ryan Weaver <ryan@thatsquality.com>
2023-01-04 11:44:39 -0500

    [tuts-apply-diff] Applying existing diff file for step debug-compose-require-debug

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
