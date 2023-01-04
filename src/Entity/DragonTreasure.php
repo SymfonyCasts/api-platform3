66b88cba59fd35441740c926ef7543268a67564f
Ryan Weaver <ryan@thatsquality.com>
2023-01-04 15:52:00 -0500

    tuts hero working...

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -61,7 +61,7 @@ class DragonTreasure
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['treasure:read', 'treasure:write', 'user:read'])]
+    #[Groups(['treasure:read', 'treasure:write', 'user:read', 'user:write'])]
     #[ApiFilter(SearchFilter::class, strategy: 'partial')]
     #[Assert\NotBlank]
     #[Assert\Length(min: 2, max: 50, maxMessage: 'Describe your loot in 50 chars or less')]
@@ -77,7 +77,7 @@ class DragonTreasure
      * The estimated value of this treasure, in gold coins.
      */
     #[ORM\Column]
-    #[Groups(['treasure:read', 'treasure:write', 'user:read'])]
+    #[Groups(['treasure:read', 'treasure:write', 'user:read', 'user:write'])]
     #[ApiFilter(RangeFilter::class)]
     #[Assert\GreaterThanOrEqual(0)]
     private ?int $value = 0;
@@ -136,7 +136,7 @@ class DragonTreasure
     }
 
     #[SerializedName('description')]
-    #[Groups(['treasure:write'])]
+    #[Groups(['treasure:write', 'user:write'])]
     public function setTextDescription(string $description): self
     {
         $this->description = nl2br($description);
