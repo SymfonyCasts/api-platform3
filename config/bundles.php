10465479f82a47f6aa5629d54ed53bbd005e076f
Ryan Weaver <ryan@thatsquality.com>
2023-01-05 10:07:37 -0500

    removing temporary bundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,6 +2,4 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfonycasts\AIMakerBundle\AIMakerBundle::class => ['all' => true],
 ];
