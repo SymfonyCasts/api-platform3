75070af72d73ad3d8a4c1f187def23b80101e785
Ryan Weaver <ryan@thatsquality.com>
2023-01-05 09:23:52 -0500

    tuts hero working...

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -53,6 +53,9 @@ use function Symfony\Component\String\u;
     paginationItemsPerPage: 10,
 )]
 #[ApiFilter(PropertyFilter::class)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'owner.username' => 'partial',
+])]
 class DragonTreasure
 {
     #[ORM\Id]
