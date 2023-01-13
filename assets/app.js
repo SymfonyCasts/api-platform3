e34578e1a21162bcfcd8a121e2d196d542bd0e59
Ryan Weaver <ryan@thatsquality.com>
2023-01-13 10:36:30 -0500

    tuts hero working...

--- assets/app.js
+++ assets/app.js
@@ -4,9 +4,11 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
+import { registerReactControllerComponents } from '@symfony/ux-react';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
