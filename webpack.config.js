30cb6d7a7298cfd1e08abeca0ac4965a29c2242d
Ryan Weaver <ryan@thatsquality.com>
2023-02-08 15:16:51 -0500

    Adding tailwind setup

--- webpack.config.js
+++ webpack.config.js
@@ -71,6 +71,10 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+    .enablePostCssLoader()
+    .copyFiles(
+        { from: './assets/images' }
+    )
 ;
 
 module.exports = Encore.getWebpackConfig();
