72c774ab30c2bfba4d68bb5ba7bfb48727090932
Ryan Weaver <ryan@thatsquality.com>
2023-02-08 15:19:05 -0500

    bootstrapping homepage

---
+++ src/Controller/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return $this->render('main/homepage.html.twig');
+    }
+}
