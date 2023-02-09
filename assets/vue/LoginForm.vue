e6aa8c1c6ac50f46e77fb62d9c55340d4b76bdc5
Ryan Weaver <ryan@thatsquality.com>
2023-02-09 10:57:28 -0500

    adding cheats for email/pass

--- assets/vue/LoginForm.vue
+++ assets/vue/LoginForm.vue
@@ -17,19 +17,21 @@
                 v-model="email"
                 type="email"
                 placeholder="Email">
+            <p class="mt-1 text-xs text-gray-500">Try: <a href="#" @click.prevent="loadEmailField">bernie@dragonmail.com</a></p>
         </div>
         <div class="mb-6">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                 Password
             </label>
             <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 :class="{ 'border-red-500': error }"
                 id="password"
                 v-model="password"
                 type="password"
                 placeholder="Password"
                 >
+            <p class="mt-1 text-xs text-gray-500">Try: <a href="#" @click.prevent="loadPasswordField">roar</a></p>
         </div>
         <div class="flex items-center justify-between">
             <button
@@ -53,6 +55,13 @@ const password = ref('');
 const error = ref('');
 const isLoading = ref(false);
 const emit = defineEmits(['user-authenticated']);
+
+const loadEmailField = () => {
+    email.value = 'bernie@dragonmail.com';
+};
+const loadPasswordField = () => {
+    password.value = 'roar';
+};
 
 const handleSubmit = async () => {
     isLoading.value = true;
