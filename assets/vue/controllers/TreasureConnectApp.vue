16ff323b2c2a5cbeffd80c52ea89fa26fcf836a6
Ryan Weaver <ryan@thatsquality.com>
2023-02-21 09:48:32 -0500

    [tuts-apply-diff] Applying existing diff file for step userinjs-try-to-pass-user-into-vue

--- assets/vue/controllers/TreasureConnectApp.vue
+++ assets/vue/controllers/TreasureConnectApp.vue
@@ -30,8 +30,9 @@ import LoginForm from '../LoginForm';
 import coinLogoPath from '../../images/coinLogo.png';
 import goldPilePath from '../../images/GoldPile.png';
 
-defineProps(['entrypoint']);
-const user = ref(window.user);
+const props = defineProps(['entrypoint', 'user'])
+console.log(props.user);
+const user = ref(props.user);
 
 const onUserAuthenticated = async (userUri) => {
     const response = await fetch(userUri);
