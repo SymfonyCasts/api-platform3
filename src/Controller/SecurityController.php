9ffdb9ccd75e3d129a6a0eab8a2236f29d2fc229
Ryan Weaver <ryan@thatsquality.com>
2023-02-21 09:48:28 -0500

    [tuts-apply-diff] Applying existing diff file for step json-login-return-user-id

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,12 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login', methods: ['POST'])]
-    public function login(): Response
+    public function login(#[CurrentUser] $user = null): Response
     {
-        
+        return $this->json([
+            'user' => $user ? $user->getId() : null,
+        ]);
     }
 }
