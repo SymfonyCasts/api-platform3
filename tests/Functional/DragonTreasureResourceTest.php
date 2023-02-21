da36e0c76a2d04737bd211b2e63ed4f8aa377b7b
Ryan Weaver <ryan@thatsquality.com>
2023-02-21 09:48:39 -0500

    [tuts-apply-diff] Applying existing diff file for step testing-first-test-with-dump

--- tests/Functional/DragonTreasureResourceTest.php
+++ tests/Functional/DragonTreasureResourceTest.php
@@ -3,8 +3,17 @@
 namespace App\Tests\Functional;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Browser\Test\HasBrowser;
 
 class DragonTreasureResourceTest extends KernelTestCase
 {
+    use HasBrowser;
 
+    public function testGetCollectionOfTreasures(): void
+    {
+        $this->browser()
+            ->get('/api/treasures')
+            ->dump()
+        ;
+    }
 }
