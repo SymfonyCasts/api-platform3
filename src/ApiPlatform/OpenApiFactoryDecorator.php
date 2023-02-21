9f8a4426ec8bca95d7093fc4831a23909f6716de
Ryan Weaver <ryan@thatsquality.com>
2023-02-21 09:48:35 -0500

    [tuts-apply-diff] Applying existing diff file for step bearer-complete-decoration

--- src/ApiPlatform/OpenApiFactoryDecorator.php
+++ src/ApiPlatform/OpenApiFactoryDecorator.php
@@ -4,11 +4,21 @@ namespace App\ApiPlatform;
 
 use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
 use ApiPlatform\OpenApi\OpenApi;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 
+#[AsDecorator('api_platform.openapi.factory')]
 class OpenApiFactoryDecorator implements OpenApiFactoryInterface
 {
+    public function __construct(private OpenApiFactoryInterface $decorated)
+    {
+    }
+
     public function __invoke(array $context = []): OpenApi
     {
-        // TODO: Implement __invoke() method.
+        $openApi = $this->decorated->__invoke($context);
+
+        dump($openApi);
+
+        return $openApi;
     }
 }
