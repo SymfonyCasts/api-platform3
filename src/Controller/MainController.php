e7f658ea9f810cb042be1ec213281f325fd69ca2
Ryan Weaver <ryan@thatsquality.com>
2023-02-21 09:48:32 -0500

    [tuts-apply-diff] Applying existing diff file for step userinjs-normalize-in-controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class MainController extends AbstractController
 {
     #[Route('/')]
-    public function homepage(): Response
+    public function homepage(NormalizerInterface $normalizer, #[CurrentUser] User $user = null): Response
     {
-        return $this->render('main/homepage.html.twig');
+        return $this->render('main/homepage.html.twig', [
+            'userData' => $normalizer->normalize($user, 'jsonld', [
+                'groups' => ['user:read'],
+            ]),
+        ]);
     }
 }
