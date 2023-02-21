bf03b18f09c0b79b04c11c197cd47e1a384dedb8
Ryan Weaver <ryan@thatsquality.com>
2023-02-21 09:48:39 -0500

    [tuts-apply-diff] Applying existing diff file for step testing-add-resetdatabase-trait

--- tests/Functional/DragonTreasureResourceTest.php
+++ tests/Functional/DragonTreasureResourceTest.php
@@ -4,10 +4,12 @@ namespace App\Tests\Functional;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Zenstruck\Browser\Test\HasBrowser;
+use Zenstruck\Foundry\Test\ResetDatabase;
 
 class DragonTreasureResourceTest extends KernelTestCase
 {
     use HasBrowser;
+    use ResetDatabase;
 
     public function testGetCollectionOfTreasures(): void
     {
