f2699d5c5604a156dd43470c45be674f335ac10b
Ryan Weaver <ryan@thatsquality.com>
2023-02-21 09:48:28 -0500

    [tuts-apply-diff] Applying existing diff file for step json-login-securitycontroller-with-login-route

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'app_login', methods: ['POST'])]
+    public function login(): Response
+    {
+        
+    }
+}
