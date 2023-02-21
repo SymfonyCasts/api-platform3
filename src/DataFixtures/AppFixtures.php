3b68e0432cab471eda7ed37fb04e6e1a57a5bab7
Ryan Weaver <ryan@thatsquality.com>
2023-02-21 09:48:34 -0500

    [tuts-apply-diff] Applying existing diff file for step apitoken-add-fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\ApiTokenFactory;
 use App\Factory\DragonTreasureFactory;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -20,6 +21,12 @@ class AppFixtures extends Fixture
         DragonTreasureFactory::createMany(40, function () {
             return [
                 'owner' => UserFactory::random(),
+            ];
+        });
+
+        ApiTokenFactory::createMany(30, function () {
+            return [
+                'ownedBy' => UserFactory::random(),
             ];
         });
     }
