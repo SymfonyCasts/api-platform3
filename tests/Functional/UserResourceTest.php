d30176a7c89014a136a29a536c93efdd36828735
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:07:52 -0400

    [tuts-apply-diff] Applying existing diff file for step password-bootstrap-user-test

---
+++ tests/Functional/UserResourceTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class UserResourceTest extends ApiTestCase
+{
+    use ResetDatabase;
+
+    public function testPostToCreateUser(): void
+    {
+
+    }
+}
