755e58b93fd6749697f2fd5532a80487239dfbe4
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:07:58 -0400

    [tuts-apply-diff] Applying existing diff file for step ownergroups-add-array-context-arg

--- src/Normalizer/AddOwnerGroupsNormalizer.php
+++ src/Normalizer/AddOwnerGroupsNormalizer.php
@@ -6,12 +6,12 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class AddOwnerGroupsNormalizer implements NormalizerInterface
 {
-    public function normalize(mixed $object, string $format = null, array $context = [])
+    public function normalize(mixed $object, string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
     {
         // TODO: Implement normalize() method.
     }
 
-    public function supportsNormalization(mixed $data, string $format = null)
+    public function supportsNormalization(mixed $data, string $format = null, array $context = []): bool
     {
         // TODO: Implement supportsNormalization() method.
     }
