b47f07ecf2d940ee48c53debf3a1c7e9446f2007
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:08:02 -0400

    [tuts-apply-diff] Applying existing diff file for step validationsecurity-inject-security

--- src/Validator/IsValidOwnerValidator.php
+++ src/Validator/IsValidOwnerValidator.php
@@ -3,11 +3,16 @@
 namespace App\Validator;
 
 use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class IsValidOwnerValidator extends ConstraintValidator
 {
+    public function __construct(private Security $security)
+    {
+    }
+
     public function validate($value, Constraint $constraint)
     {
         assert($constraint instanceof IsValidOwner);
@@ -18,6 +23,11 @@ class IsValidOwnerValidator extends ConstraintValidator
 
         // constraint is only meant to be used above a User property
         assert($value instanceof User);
+
+        $user = $this->security->getUser();
+        if (!$user) {
+            throw new \LogicException('IsOwnerValidator should only be used when a user is logged in.');
+        }
 
         $this->context->buildViolation($constraint->message)
             ->addViolation();
