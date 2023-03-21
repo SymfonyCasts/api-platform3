cfd0d96a11467f251cd97c05d04780c2943d4086
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:07:54 -0400

    [tuts-apply-diff] Applying existing diff file for step stateprocessor-erasecredentials

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -187,7 +187,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function eraseCredentials()
     {
         // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+        $this->plainPassword = null;
     }
 
     public function getUsername(): ?string
