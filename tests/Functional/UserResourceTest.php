7557fb59f92d959120a6f443ec30713215be9767
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:08:03 -0400

    [tuts-apply-diff] Applying existing diff file for step embeddedsecurity-add-failing-test

--- tests/Functional/UserResourceTest.php
+++ tests/Functional/UserResourceTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional;
 
+use App\Factory\DragonTreasureFactory;
 use App\Factory\UserFactory;
 use Zenstruck\Foundry\Test\ResetDatabase;
 
@@ -43,5 +44,25 @@ class UserResourceTest extends ApiTestCase
                 'headers' => ['Content-Type' => 'application/merge-patch+json']
             ])
             ->assertStatus(200);
+    }
+
+    public function testTreasuresCannotBeStolen(): void
+    {
+        $user = UserFactory::createOne();
+        $otherUser = UserFactory::createOne();
+        $dragonTreasure = DragonTreasureFactory::createOne(['owner' => $otherUser]);
+
+        $this->browser()
+            ->actingAs($user)
+            ->patch('/api/users/' . $user->getId(), [
+                'json' => [
+                    'username' => 'changed',
+                    'dragonTreasures' => [
+                        '/api/treasures/' . $dragonTreasure->getId(),
+                    ],
+                ],
+                'headers' => ['Content-Type' => 'application/merge-patch+json']
+            ])
+            ->assertStatus(422);
     }
 }
