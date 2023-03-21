1a062bd7b9a64da060b0edb9d54fa3a2212a967d
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:08:01 -0400

    [tuts-apply-diff] Applying existing diff file for step validationsecurity-make-validator

---
+++ src/Validator/IsValidOwner.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ *
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
+class IsValidOwner extends Constraint
+{
+    /*
+     * Any public properties become valid options for the annotation.
+     * Then, use these in your validator class.
+     */
+    public $message = 'The value "{{ value }}" is not valid.';
+}
