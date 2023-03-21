acd996921a02147da7a6436ae2aa8e5a346173ce
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:07:59 -0400

    [tuts-apply-diff] Applying existing diff file for step ownergroups-add-serializerawareinterface

--- src/Normalizer/AddOwnerGroupsNormalizer.php
+++ src/Normalizer/AddOwnerGroupsNormalizer.php
@@ -4,9 +4,11 @@ namespace App\Normalizer;
 
 use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerAwareInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[AsDecorator('api_platform.jsonld.normalizer.item')]
-class AddOwnerGroupsNormalizer implements NormalizerInterface
+class AddOwnerGroupsNormalizer implements NormalizerInterface, SerializerAwareInterface
 {
     public function __construct(private NormalizerInterface $normalizer)
     {
@@ -22,5 +24,12 @@ class AddOwnerGroupsNormalizer implements NormalizerInterface
     public function supportsNormalization(mixed $data, string $format = null, array $context = []): bool
     {
         return $this->normalizer->supportsNormalization($data, $format);
+    }
+
+    public function setSerializer(SerializerInterface $serializer)
+    {
+        if ($this->normalizer instanceof SerializerAwareInterface) {
+            $this->normalizer->setSerializer($serializer);
+        }
     }
 }
