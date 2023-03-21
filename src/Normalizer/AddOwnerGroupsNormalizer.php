a72687069011877d8182a623154a8ad8c8c12cb8
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:07:58 -0400

    [tuts-apply-diff] Applying existing diff file for step ownergroups-set-up-the-decoration

--- src/Normalizer/AddOwnerGroupsNormalizer.php
+++ src/Normalizer/AddOwnerGroupsNormalizer.php
@@ -6,13 +6,19 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class AddOwnerGroupsNormalizer implements NormalizerInterface
 {
+    public function __construct(private NormalizerInterface $normalizer)
+    {
+    }
+
     public function normalize(mixed $object, string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
     {
-        // TODO: Implement normalize() method.
+        dump('IT WORKS!');
+
+        return $this->normalizer->normalize($object, $format, $context);
     }
 
     public function supportsNormalization(mixed $data, string $format = null, array $context = []): bool
     {
-        // TODO: Implement supportsNormalization() method.
+        return $this->normalizer->supportsNormalization($data, $format);
     }
 }
