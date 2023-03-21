1deeaa2cd31f67fcc47a24b1a4693a80f7274bf6
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:08:05 -0400

    [tuts-apply-diff] Applying existing diff file for step autosetowner-set-up-decoration

--- src/State/DragonTreasureSetOwnerProcessor.php
+++ src/State/DragonTreasureSetOwnerProcessor.php
@@ -4,11 +4,17 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 
+#[AsDecorator('api_platform.doctrine.orm.state.persist_processor')]
 class DragonTreasureSetOwnerProcessor implements ProcessorInterface
 {
+    public function __construct(private ProcessorInterface $innerProcessor)
+    {
+    }
+
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
     {
-        // Handle the state
+        $this->innerProcessor->process($data, $operation, $uriVariables, $context);
     }
 }
