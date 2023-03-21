c14410660d5e5d9629914c4970f3e276169ae7b3
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:08:06 -0400

    [tuts-apply-diff] Applying existing diff file for step queryextension-complete-logic

--- src/ApiPlatform/DragonTreasureIsPublishedExtension.php
+++ src/ApiPlatform/DragonTreasureIsPublishedExtension.php
@@ -5,12 +5,19 @@ namespace App\ApiPlatform;
 use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Metadata\Operation;
+use App\Entity\DragonTreasure;
 use Doctrine\ORM\QueryBuilder;
 
 class DragonTreasureIsPublishedExtension implements QueryCollectionExtensionInterface
 {
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = []): void
     {
-        // TODO: Implement applyToCollection() method.
+        if (DragonTreasure::class !== $resourceClass) {
+            return;
+        }
+
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder->andWhere(sprintf('%s.isPublished = :isPublished', $rootAlias))
+            ->setParameter('isPublished', true);
     }
 }
