58a5c6d6cf72a5b3f7d5e374776e217849af1a4c
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:07:57 -0400

    [tuts-apply-diff] Applying existing diff file for step dynamicgroups-implement-decoration

--- src/ApiPlatform/AdminGroupsContextBuilder.php
+++ src/ApiPlatform/AdminGroupsContextBuilder.php
@@ -7,8 +7,15 @@ use Symfony\Component\HttpFoundation\Request;
 
 class AdminGroupsContextBuilder implements SerializerContextBuilderInterface
 {
+    public function __construct(private SerializerContextBuilderInterface $decorated)
+    {
+    }
+
     public function createFromRequest(Request $request, bool $normalization, array $extractedAttributes = null): array
     {
-        // TODO: Implement createFromRequest() method.
+        $context = $this->decorated->createFromRequest($request, $normalization, $extractedAttributes);
+        dump('I AM WORKING!');
+
+        return $context;
     }
 }
