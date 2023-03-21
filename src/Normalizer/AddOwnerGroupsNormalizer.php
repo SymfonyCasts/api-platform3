70604e7feab5e4e3e45d457674968e2e3a9650b1
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:07:58 -0400

    [tuts-apply-diff] Applying existing diff file for step ownergroups-create-normalizer-class

---
+++ src/Normalizer/AddOwnerGroupsNormalizer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class AddOwnerGroupsNormalizer implements NormalizerInterface
+{
+    public function normalize(mixed $object, string $format = null, array $context = [])
+    {
+        // TODO: Implement normalize() method.
+    }
+
+    public function supportsNormalization(mixed $data, string $format = null)
+    {
+        // TODO: Implement supportsNormalization() method.
+    }
+}
