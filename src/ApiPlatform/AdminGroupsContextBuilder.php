2ced908add3d11c6cb731be6259d3dd3879b9ed2
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:07:57 -0400

    [tuts-apply-diff] Applying existing diff file for step dynamicgroups-add-asdecorator

--- src/ApiPlatform/AdminGroupsContextBuilder.php
+++ src/ApiPlatform/AdminGroupsContextBuilder.php
@@ -3,8 +3,10 @@
 namespace App\ApiPlatform;
 
 use ApiPlatform\Serializer\SerializerContextBuilderInterface;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 use Symfony\Component\HttpFoundation\Request;
 
+#[AsDecorator('api_platform.serializer.context_builder')]
 class AdminGroupsContextBuilder implements SerializerContextBuilderInterface
 {
     public function __construct(private SerializerContextBuilderInterface $decorated)
