e19cf1f546ff4adc96d1c86641525b437264a4f0
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:08:00 -0400

    [tuts-apply-diff] Applying existing diff file for step validationsecurity-remove-securitypostdenormalize

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -41,7 +41,6 @@ use function Symfony\Component\String\u;
         ),
         new Patch(
             security: 'is_granted("EDIT", object)',
-            securityPostDenormalize: 'is_granted("EDIT", object)',
         ),
         new Delete(
             security: 'is_granted("ROLE_ADMIN")',
