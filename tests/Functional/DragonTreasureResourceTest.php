53075dc60fb7ad8928d605c3dc9e69b795ab52b3
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:08:05 -0400

    [tuts-apply-diff] Applying existing diff file for step queryextension-adjust-test

--- tests/Functional/DragonTreasureResourceTest.php
+++ tests/Functional/DragonTreasureResourceTest.php
@@ -16,7 +16,12 @@ class DragonTreasureResourceTest extends ApiTestCase
 
     public function testGetCollectionOfTreasures(): void
     {
-        DragonTreasureFactory::createMany(5);
+        DragonTreasureFactory::createMany(5, [
+            'isPublished' => true,
+        ]);
+        DragonTreasureFactory::createOne([
+            'isPublished' => false,
+        ]);
 
         $json = $this->browser()
             ->get('/api/treasures')
