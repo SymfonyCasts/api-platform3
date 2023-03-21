3bee54d2888d85f70729e8f4a705f40cb9f20be0
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:08:04 -0400

    [tuts-apply-diff] Applying existing diff file for step autosetowner-remove-validation-constraint

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -134,7 +134,6 @@ class DragonTreasure
     #[Groups(['treasure:read', 'treasure:write'])]
     #[Assert\Valid]
     #[IsValidOwner]
-    #[Assert\NotNull]
     #[ApiFilter(SearchFilter::class, strategy: 'exact')]
     private ?User $owner = null;
 
