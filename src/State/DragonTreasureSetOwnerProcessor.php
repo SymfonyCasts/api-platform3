7e19f613f7ed1e81065b1896d12bc830fc76f4f8
Ryan Weaver <ryan@thatsquality.com>
2023-03-21 09:08:05 -0400

    [tuts-apply-diff] Applying existing diff file for step autosetowner-complete-processor

--- src/State/DragonTreasureSetOwnerProcessor.php
+++ src/State/DragonTreasureSetOwnerProcessor.php
@@ -4,17 +4,23 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use App\Entity\DragonTreasure;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 
 #[AsDecorator('api_platform.doctrine.orm.state.persist_processor')]
 class DragonTreasureSetOwnerProcessor implements ProcessorInterface
 {
-    public function __construct(private ProcessorInterface $innerProcessor)
+    public function __construct(private ProcessorInterface $innerProcessor, private Security $security)
     {
     }
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
     {
+        if ($data instanceof DragonTreasure && $data->getOwner() === null && $this->security->getUser()) {
+            $data->setOwner($this->security->getUser());
+        }
+
         $this->innerProcessor->process($data, $operation, $uriVariables, $context);
     }
 }
