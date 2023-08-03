053b851eb702e970287f9686068306794e8c70e2
Ryan Weaver <ryan@symfonycasts.com>
2023-08-03 14:45:16 -0400

    Adding Notification entity

---
+++ src/Entity/Notification.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NotificationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NotificationRepository::class)]
+class Notification
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?DragonTreasure $dragonTreasure = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $message = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDragonTreasure(): ?DragonTreasure
+    {
+        return $this->dragonTreasure;
+    }
+
+    public function setDragonTreasure(?DragonTreasure $dragonTreasure): static
+    {
+        $this->dragonTreasure = $dragonTreasure;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
