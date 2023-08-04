261465e2bb6eb6f98d283e5807c2d542feb95a8d
Ryan Weaver <ryan@symfonycasts.com>
2023-08-04 13:47:01 -0400

    [tuts-apply-diff] Applying existing diff file for step customfieldstateprovider-autowire-attribute

--- src/State/DragonTreasureStateProvider.php
+++ src/State/DragonTreasureStateProvider.php
@@ -4,10 +4,13 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class DragonTreasureStateProvider implements ProviderInterface
 {
-    public function __construct(private ProviderInterface $itemProvider)
+    public function __construct(
+        #[Autowire(service: 'api_platform.doctrine.orm.state.item_provider')] private ProviderInterface $itemProvider
+    )
     {
     }
 
