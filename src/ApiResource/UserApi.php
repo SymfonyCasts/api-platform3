7f9468ab86fd5698cb252640dacdbf9ab740dd20
Ryan Weaver <ryan@symfonycasts.com>
2023-08-04 13:47:06 -0400

    [tuts-apply-diff] Applying existing diff file for step stateoptions-add-a-filter

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -2,7 +2,9 @@
 
 namespace App\ApiResource;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use App\Entity\DragonTreasure;
 use App\Entity\User;
@@ -13,6 +15,9 @@ use Doctrine\Common\Collections\Collection;
     paginationItemsPerPage: 5,
     stateOptions: new Options(entityClass: User::class),
 )]
+#[ApiFilter(SearchFilter::class, properties: [
+    'username' => 'partial',
+])]
 class UserApi
 {
     public ?int $id = null;
