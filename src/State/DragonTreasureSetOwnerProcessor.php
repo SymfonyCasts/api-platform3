dd4ef4085e0e5fc9bcbf1a8fd33bf37161ce20d1
Ryan Weaver <ryan@symfonycasts.com>
2023-08-04 13:47:03 -0400

    [tuts-apply-diff] Applying existing diff file for step customfieldstateprovider-set-field-also-in-processor

--- src/State/DragonTreasureSetOwnerProcessor.php
+++ src/State/DragonTreasureSetOwnerProcessor.php
@@ -22,5 +22,9 @@ class DragonTreasureSetOwnerProcessor implements ProcessorInterface
         }
 
         $this->innerProcessor->process($data, $operation, $uriVariables, $context);
+
+        if ($data instanceof DragonTreasure) {
+            $data->setIsOwnedByAuthenticatedUser($data->getOwner() === $this->security->getUser());
+        }
     }
 }
