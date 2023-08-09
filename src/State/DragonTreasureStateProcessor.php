3501c222356fe7343d99aafb39cce85a92ad9eaf
Ryan Weaver <ryan@symfonycasts.com>
2023-08-09 14:29:33 -0400

    [tuts-apply-diff] Applying existing diff file for step publishprocessor-check-for-publish-change

--- src/State/DragonTreasureStateProcessor.php
+++ src/State/DragonTreasureStateProcessor.php
@@ -20,13 +20,20 @@ class DragonTreasureStateProcessor implements ProcessorInterface
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
     {
-        dd($context);
         assert($data instanceof DragonTreasure);
         $data->setOwner($this->security->getUser());
 
         $data = $this->innerProcessor->process($data, $operation, $uriVariables, $context);
 
         $data->setIsOwnedByAuthenticatedUser($data->getOwner() === $this->security->getUser());
+
+        $previousData = $context['previous_data'] ?? null;
+        if ($previousData instanceof DragonTreasure
+            && $data->getIsPublished()
+            && $previousData->getIsPublished() !== $data->getIsPublished()
+        ) {
+            dd('published!');
+        }
 
         return $data;
     }
