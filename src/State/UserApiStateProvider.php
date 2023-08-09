c119520c6b1c5ab8978b738a8f08ce112cd3ea60
Ryan Weaver <ryan@symfonycasts.com>
2023-08-09 14:29:38 -0400

    [tuts-apply-diff] Applying existing diff file for step providerlogic-return-traversablepaginator

--- src/State/UserApiStateProvider.php
+++ src/State/UserApiStateProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\State;
 
+use ApiPlatform\Doctrine\Orm\Paginator;
 use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
 use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\Pagination\TraversablePaginator;
 use ApiPlatform\State\ProviderInterface;
 use App\ApiResource\UserApi;
 use App\Entity\User;
@@ -21,13 +23,19 @@ class UserApiStateProvider implements ProviderInterface
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
         $users = $this->collectionProvider->provide($operation, $uriVariables, $context);
+        assert($users instanceof Paginator);
 
         $userDtos = [];
         foreach ($users as $user) {
             $userDtos[] = $this->mapEntityToDto($user);
         }
 
-        return $userDtos;
+        return new TraversablePaginator(
+            new \ArrayIterator($userDtos),
+            $users->getCurrentPage(),
+            $users->getItemsPerPage(),
+            $users->getTotalItems()
+        );
     }
 
     private function mapEntityToDto(User $user): UserApi
