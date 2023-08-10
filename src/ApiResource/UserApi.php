e9bdc42038d36bff80a656dd157301901c8cd33e
Ryan Weaver <ryan@symfonycasts.com>
2023-08-10 14:18:26 -0400

    tuts hero working...

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -7,6 +7,12 @@ use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Entity\DragonTreasure;
 use App\State\UserApiStateProcessor;
 use App\State\UserApiStateProvider;
@@ -17,6 +23,16 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 #[ApiResource(
     shortName: 'User',
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(
+            validationContext: ['groups' => ['Default', 'postValidation']],
+        ),
+        new Put(),
+        new Patch(),
+        new Delete(),
+    ],
     paginationItemsPerPage: 5,
     provider: UserApiStateProvider::class,
     processor: UserApiStateProcessor::class,
@@ -34,12 +50,14 @@ class UserApi
     #[Assert\Email]
     public ?string $email = null;
 
+    #[Assert\NotBlank]
     public ?string $username = null;
 
     /**
      * The plaintext password when being set or changed.
      */
     #[ApiProperty(readable: false)]
+    #[Assert\NotBlank(groups: ['postValidation'])]
     public ?string $password = null;
 
     /**
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -84,17 +84,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[Groups(['user:write'])]
     #[SerializedName('password')]
-    #[Assert\NotBlank(groups: ['postValidation'])]
     private ?string $plainPassword = null;
 
     #[ORM\Column(length: 255, unique: true)]
     #[Groups(['user:read', 'user:write', 'treasure:item:get', 'treasure:write'])]
-    #[Assert\NotBlank]
     private ?string $username = null;
 
     #[ORM\OneToMany(mappedBy: 'owner', targetEntity: DragonTreasure::class, cascade: ['persist'], orphanRemoval: true)]
     #[Groups(['user:write'])]
-    #[Assert\Valid]
     #[TreasuresAllowedOwnerChange]
     private Collection $dragonTreasures;
 
