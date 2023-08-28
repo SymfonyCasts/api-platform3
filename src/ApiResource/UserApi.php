735c4bbd8c44e07c6a63e237e69f836770b2bfda
Ryan Weaver <ryan@symfonycasts.com>
2023-08-28 16:04:41 -0400

    Step 80: DtoProvider: add provider option

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -7,11 +7,13 @@ use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use App\Entity\DragonTreasure;
+use App\State\EntityClassDtoStateProvider;
 use App\Entity\User;
 
 #[ApiResource(
     shortName: 'User',
     paginationItemsPerPage: 5,
+    provider: EntityClassDtoStateProvider::class,
     stateOptions: new Options(entityClass: User::class),
 )]
 #[ApiFilter(SearchFilter::class, properties: [
