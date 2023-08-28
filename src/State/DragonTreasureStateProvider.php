920d82f8e4d0a08f962dceebaef1dd695561f5f3
Ryan Weaver <ryan@symfonycasts.com>
2023-08-28 16:04:25 -0400

    Step 6: CustomFieldStateProvider: add dd()

--- src/State/DragonTreasureStateProvider.php
+++ src/State/DragonTreasureStateProvider.php
@@ -9,6 +9,6 @@ class DragonTreasureStateProvider implements ProviderInterface
 {
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
-        // Retrieve the state from somewhere
+        dd($operation);
     }
 }
