8d5dbb69631da348b7b944bd8470fc72b36672f1
Ryan Weaver <ryan@symfonycasts.com>
2023-08-28 16:04:35 -0400

    Step 52: CustomResourceRelations: Add treasures to test

--- tests/Functional/DailyQuestResourceTest.php
+++ tests/Functional/DailyQuestResourceTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional;
 
+use App\Factory\DragonTreasureFactory;
 use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Foundry\Test\ResetDatabase;
 
@@ -12,6 +13,9 @@ class DailyQuestResourceTest extends ApiTestCase
 
     public function testPatchCanUpdateStatus()
     {
+        // quests need at least some treasures to be available
+        DragonTreasureFactory::createMany(5);
+
         $yesterday = new \DateTime('-1 day');
         $this->browser()
             ->patch('/api/quests/'.$yesterday->format('Y-m-d'), [
