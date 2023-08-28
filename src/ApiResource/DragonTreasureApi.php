ac4391ab09d6e630b4827110b90d1b81db2905f3
Ryan Weaver <ryan@symfonycasts.com>
2023-08-28 16:04:53 -0400

    Step 132: Treasure: Create DragonTreasure resource class

---
+++ src/ApiResource/DragonTreasureApi.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\ApiResource;
+
+use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use App\Entity\DragonTreasure;
+use App\State\EntityClassDtoStateProcessor;
+use App\State\EntityClassDtoStateProvider;
+
+#[ApiResource(
+    shortName: 'Treasure',
+    paginationItemsPerPage: 10,
+    provider: EntityClassDtoStateProvider::class,
+    processor: EntityClassDtoStateProcessor::class,
+    stateOptions: new Options(entityClass: DragonTreasure::class),
+)]
+class DragonTreasureApi
+{
+    #[ApiProperty(readable: false, writable: false, identifier: true)]
+    public ?int $id = null;
+
+    public ?string $name = null;
+}
