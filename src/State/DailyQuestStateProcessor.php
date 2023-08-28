9b2f499bebd9a3e7f95abeb81401ccbbffbccc3e
Ryan Weaver <ryan@symfonycasts.com>
2023-08-28 16:04:35 -0400

    Step 49: CustomResourceProcessor: set lastUpdated from processor

--- src/State/DailyQuestStateProcessor.php
+++ src/State/DailyQuestStateProcessor.php
@@ -4,11 +4,14 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use App\ApiResource\DailyQuest;
 
 class DailyQuestStateProcessor implements ProcessorInterface
 {
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
     {
-        dd($data);
+        assert($data instanceof DailyQuest);
+
+        $data->lastUpdated = new \DateTimeImmutable('now');
     }
 }
