66523669ff485d8668bc34e81c7edd037a2e99df
Ryan Weaver <ryan@symfonycasts.com>
2023-08-28 16:04:50 -0400

    Step 121: AutoMapper: Implement init() & dd

--- src/Mapper/UserApiToEntityMapper.php
+++ src/Mapper/UserApiToEntityMapper.php
@@ -4,20 +4,34 @@ namespace App\Mapper;
 
 use App\ApiResource\UserApi;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfonycasts\MicroMapper\AsMapper;
 use Symfonycasts\MicroMapper\MapperInterface;
 
 #[AsMapper(from: UserApi::class, to: User::class)]
 class UserApiToEntityMapper implements MapperInterface
 {
+    public function __construct(
+        private UserRepository $userRepository,
+    )
+    {
+    }
+
     public function load(object $from, string $toClass, array $context): object
     {
-        dd($from, $toClass);
-        // TODO: Implement load() method.
+        $dto = $from;
+        assert($dto instanceof UserApi);
+
+        $userEntity = $dto->id ? $this->userRepository->find($dto->id) : new User();
+        if (!$userEntity) {
+            throw new \Exception('User not found');
+        }
+
+        return $userEntity;
     }
 
     public function populate(object $from, object $to, array $context): object
     {
-        // TODO: Implement populate() method.
+        dd($from, $to);
     }
 }
