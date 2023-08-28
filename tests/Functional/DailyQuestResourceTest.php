1a30d3ff14704696b5e01fa0afd4b0936eca64b5
Ryan Weaver <ryan@symfonycasts.com>
2023-08-28 16:04:34 -0400

    Step 45: CustomResourceProcessor: Add PATCH test

--- tests/Functional/DailyQuestResourceTest.php
+++ tests/Functional/DailyQuestResourceTest.php
@@ -9,4 +9,20 @@ class DailyQuestResourceTest extends ApiTestCase
 {
     use ResetDatabase;
     use Factories;
+
+    public function testPatchCanUpdateStatus()
+    {
+        $yesterday = new \DateTime('-1 day');
+        $this->browser()
+            ->patch('/api/quests/'.$yesterday->format('Y-m-d'), [
+                'json' => [
+                    'status' => 'completed',
+                ],
+                'headers' => ['Content-Type' => 'application/merge-patch+json']
+            ])
+            ->assertStatus(200)
+            ->dump()
+            ->assertJsonMatches('status', 'completed')
+        ;
+    }
 }
