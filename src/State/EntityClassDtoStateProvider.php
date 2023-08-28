dff0d444a4db65569846a1d3dd541d3ad098a21c
Ryan Weaver <ryan@symfonycasts.com>
2023-08-28 16:04:41 -0400

    Step 81: ProviderLogic: Query & Map to DTO

--- src/State/EntityClassDtoStateProvider.php
+++ src/State/EntityClassDtoStateProvider.php
@@ -2,13 +2,23 @@
 
 namespace App\State;
 
+use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class EntityClassDtoStateProvider implements ProviderInterface
 {
+    public function __construct(
+        #[Autowire(service: CollectionProvider::class)] private ProviderInterface $collectionProvider
+    )
+    {
+
+    }
+
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
-        // Retrieve the state from somewhere
+        $entities = $this->collectionProvider->provide($operation, $uriVariables, $context);
+        dd($entities);
     }
 }
