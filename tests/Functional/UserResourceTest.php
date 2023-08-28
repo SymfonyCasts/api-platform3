8437e68da6b7e929961631065da7a34b9730e2f3
Ryan Weaver <ryan@symfonycasts.com>
2023-08-28 16:05:04 -0400

    Step 183: RelationsWrite: remove dump() in test

--- tests/Functional/UserResourceTest.php
+++ tests/Functional/UserResourceTest.php
@@ -83,7 +83,6 @@ class UserResourceTest extends ApiTestCase
         $user = UserFactory::createOne();
         $otherUser = UserFactory::createOne();
         $dragonTreasure = DragonTreasureFactory::createOne(['owner' => $otherUser]);
-        dump('Real owner is ' . $otherUser->getId());
 
         $this->browser()
             ->actingAs($user)
