f2abca65e936dc5dc9949e6e90a18b7f4e880457
Ryan Weaver <ryan@symfonycasts.com>
2023-08-28 16:04:50 -0400

    Step 120: AutoMapper: create UserApiToEntityMapper

---
+++ src/Mapper/UserApiToEntityMapper.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Mapper;
+
+use App\ApiResource\UserApi;
+use App\Entity\User;
+use Symfonycasts\MicroMapper\AsMapper;
+use Symfonycasts\MicroMapper\MapperInterface;
+
+#[AsMapper(from: UserApi::class, to: User::class)]
+class UserApiToEntityMapper implements MapperInterface
+{
+    public function load(object $from, string $toClass, array $context): object
+    {
+        dd($from, $toClass);
+        // TODO: Implement load() method.
+    }
+
+    public function populate(object $from, object $to, array $context): object
+    {
+        // TODO: Implement populate() method.
+    }
+}
