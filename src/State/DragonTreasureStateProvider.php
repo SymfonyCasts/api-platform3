445a82f309e5fac225e696a363a3b7e2d6fe4634
Ryan Weaver <ryan@symfonycasts.com>
2024-01-12 06:34:39 -0500

    [tuts-apply-diff] Applying existing diff file for step customfieldstateprovider-set-field-but-hardcoded

--- src/State/DragonTreasureStateProvider.php
+++ src/State/DragonTreasureStateProvider.php
@@ -5,6 +5,7 @@ namespace App\State;
 use ApiPlatform\Doctrine\Orm\State\ItemProvider;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
+use App\Entity\DragonTreasure;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class DragonTreasureStateProvider implements ProviderInterface
@@ -17,6 +18,14 @@ class DragonTreasureStateProvider implements ProviderInterface
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
-        dd($operation);
+        $treasure = $this->itemProvider->provide($operation, $uriVariables, $context);
+
+        if (!$treasure instanceof DragonTreasure) {
+            return $treasure;
+        }
+
+        $treasure->setIsOwnedByAuthenticatedUser(true);
+
+        return $treasure;
     }
 }
