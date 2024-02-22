06c39e607d9355a488ed5fc9d4d162d3878c2bd7
Ryan Weaver <ryan@symfonycasts.com>
2024-02-22 09:34:01 -0500

    [tuts-apply-diff] Applying existing diff file for step stateoptions-set-provider-to-orm-collection

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -2,10 +2,12 @@
 
 namespace App\ApiResource;
 
+use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
 use ApiPlatform\Metadata\ApiResource;
 
 #[ApiResource(
     shortName: 'User',
+    provider: CollectionProvider::class,
 )]
 class UserApi
 {
