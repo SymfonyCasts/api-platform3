1c153c635cf579044b4f4c59043cd0a7bb4ec369
Ryan Weaver <ryan@symfonycasts.com>
2024-02-22 09:33:55 -0500

    [tuts-apply-diff] Applying existing diff file for step customresourceprocessor-create-test-class

---
+++ tests/Functional/DailyQuestResourceTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class DailyQuestResourceTest extends ApiTestCase
+{
+    use ResetDatabase;
+    use Factories;
+}
